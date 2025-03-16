4184963f86bebbb9dd8505bbdd3b780fce206308
Aaron <aarondeo30@gmail.com>
2025-03-16 14:55:50 -0700

    fixed audio bug

--- Studio5/Assets/_Scripts/AudioManager.cs
+++ Studio5/Assets/_Scripts/AudioManager.cs
@@ -23,14 +23,4 @@ public class AudioManager : MonoBehaviour
         audioSource.PlayOneShot(clip);
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
